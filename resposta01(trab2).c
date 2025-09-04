ef349e2cc18b5835c8e07a1019253e9049efb0da
marcelojunior03 <marceloheiffigjr@hotmail.com>
2025-09-04 15:08:22 -0300

    Update resposta01(trab2).c

--- resposta01(trab2).c
+++ resposta01(trab2).c
@@ -6,18 +6,47 @@
 
 void main(){
     int Matriz[LIN][COL];
-    int lin, col;
+    int lin, col, linNula, colNula;
 
     for(lin = 0; lin < LIN; lin++){
         for(col = 0; col < COL; col++){
-            printf("Digite um número para a linha %i, coluna %i: ", lin++, col++);
+            printf("Digite um numero para a linha %i, coluna %i: ", lin+1, col+1);
             scanf("%i", &Matriz[lin][col]);
         }
     }
 
+    linNula = 0;
+    colNula = 0;
+
     for(lin = 0; lin < LIN; lin++){
         for(col = 0; col < COL; col++){
-            
+            if(Matriz[0][col]){
+                linNula++;
+            }
+            else if(Matriz[1][col]){
+                linNula++;
+            }
+            else if(Matriz[2][col]){
+                linNula++;
+            }
+            else if(Matriz[3][col]){
+                linNula++;
+            }
+
+            if(Matriz[lin][0]){
+                colNula++;
+            }
+            else if(Matriz[lin][1]){
+                colNula++;
+            }
+            else if(Matriz[lin][2]){
+                colNula++;
+            }
+            else if(Matriz[lin][3]){
+                colNula++;
+            }
         }
     }
+
+    printf("A matriz possui %i linhas nulas e %i colunas nulas.", linNula, colNula);
 }
