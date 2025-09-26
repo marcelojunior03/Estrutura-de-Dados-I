45287145ff81f250972b409b49906c70d94c548c
marcelojunior03 <marceloheiffigjr@hotmail.com>
2025-09-26 17:53:40 -0300

    Update Trabalho04_1.c

--- Trabalho4/Trabalho04_1.c
+++ Trabalho4/Trabalho04_1.c
@@ -1,5 +1,16 @@
-//Nome: Marcelo Heiffig Júnior - RA: CV3095126
+//Nome: Marcelo Heiffig JÃºnior - RA: CV3095126
 
 #include <stdio.h>
 
-void main(){}
+void main(){
+    char senha[1][20];
+
+    printf("Digite sua senha: ");
+    
+
+    printf("A senha precisa ter no minimo 10 caracteres!");
+    printf("A senha precisa ter no minimo 2 letras maiusculas!");
+    printf("A senha precisa ter no minimo 3 letras minusculas!");
+    printf("A senha precisa ter no minimo 3 numeros!");
+    printf("A senha precisa ter no minimo 2 caracteres especiais!");
+}
