165eb75f107b64b09ef41c2e47e71250d766748e
marcelojunior03 <marceloheiffigjr@hotmail.com>
2025-09-26 22:31:03 -0300

    Update Trabalho04_2.c

--- Trabalho4/Trabalho04_2.c
+++ Trabalho4/Trabalho04_2.c
@@ -1,32 +1,36 @@
-//Nome: Marcelo Heiffig Júnior - RA: CV3095126
+//Nome: Marcelo Heiffig JÃºnior - RA: CV3095126
 
 #include <stdio.h>
+#include <string.h>
 
 #define ALUNOS 35
 #define DISCIPLINAS 6
 
 void main(){
     char alunos[ALUNOS][50];
-    char disciplinas[DISCIPLINAS][40] = {"Banco de Dados",
-                                         "Estatistica",
-                                         "Estrutura de Dados 1",
-                                         "Administracao",
-                                         "Ling. Programacao 1",
-                                         "Sistemas Operacionais"};
+    char disciplinas[DISCIPLINAS][40];
     float boletim[ALUNOS+1][DISCIPLINAS+1];
-    int linha, coluna;
+    int lin, col;
 
-    for(linha = 0; linha < ALUNOS; linha++){
+    for(lin = 0; lin < ALUNOS; lin++){
         fflush(stdin);
-        printf("\nDigite o nome do aluno %i: ", linha+1);
-        gets(alunos[linha]);
+        printf("\nDigite o nome do aluno %i: ", lin+1);
+        gets(alunos[lin]);
+    }
+    
+    for(lin = 0; lin < DISCIPLINAS; lin++){
+        fflush(stdin);
+        printf("\nDigite o nome da disciplina %i: ", lin+1);
+        gets(disciplinas[lin]);
     }
 
-    for(linha = 0; linha < ALUNOS; linha++){
-        printf("\nAluno: %s", alunos[linha]);
-        for(coluna = 0; coluna < DISCIPLINAS; coluna++){
-            printf("\nDigite a nota da disciplina %s: ", disciplinas[coluna]);
-            scanf("%f", &boletim[linha][coluna]);
+    for(lin = 0; lin < ALUNOS; lin++){
+        printf("\nAluno: %s", alunos[lin]);
+        for(col = 0; col < DISCIPLINAS; col++){
+            printf("\nDigite a nota da disciplina %s: ", disciplinas[col]);
+            scanf("%f", &boletim[lin][col]);
         }
     }
+    
+    
 }
