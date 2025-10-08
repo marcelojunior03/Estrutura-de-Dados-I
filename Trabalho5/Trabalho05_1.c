bee2387f2341f24a85ddab0a9ade267594b19b67
marcelojunior03 <marceloheiffigjr@hotmail.com>
2025-10-08 18:04:45 -0300

    Add files via upload

---
+++ Trabalho5/Trabalho05_1.c
@@ -0,0 +1,14 @@
+//Nome: Marcelo Heiffig Júnior - RA: CV3095126
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+void main(){
+    struct jogadores{
+        char nome[50];
+        int pontuacao = 0;
+    };
+
+    char resultado[5][5];
+}
