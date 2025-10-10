a017e366f762ec172d458d3132acbb51e6b16626
marcelojunior03 <marceloheiffigjr@hotmail.com>
2025-10-10 17:26:13 -0300

    Update Trabalho05_1.c

--- Trabalho5/Trabalho05_1.c
+++ Trabalho5/Trabalho05_1.c
@@ -2,13 +2,69 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include <stdbool.h>
 
 void main(){
-    struct jogadores{
+    struct jogador{
         char nome[50];
-        int pontuacao = 0;
+        int pontuacao;
     };
+    typedef struct jogador Jogador;
 
+    Jogador jogadores[5];
     char resultado[5][5];
+    char result;
+    int lin, col;
+    int contador = 0;
+    bool erro;
+
+    for(lin = 0; lin < 5; lin++){
+        fflush(stdin);
+        printf("\nDigite o nome do jogador %i: ", lin+1);
+        gets(jogadores[lin].nome);
+
+        jogadores[lin].pontuacao = 0;
+    }
+
+    for(lin = 0; lin < 5; lin++){
+        for(col = 0; col < 5; col++){
+            if(lin == col){
+                resultado[lin][col] = 'X';
+            }
+            else{
+                erro = true;
+
+                do{
+                    fflush(stdin);
+                    printf("\nDigite o resultado de %s x %s (V, E ou D): ", jogadores[lin].nome, jogadores[col].nome);
+                    result = getchar();
+                    result = toupper(result);
+                    resultado[lin][col] = result;
+
+                    if(resultado[lin][col] == 'V'){
+                        jogadores[lin].pontuacao += 10;
+                        erro = false;
+                    }
+                    else if(resultado[lin][col] == 'E'){
+                        jogadores[lin].pontuacao += 5;
+                        erro = false;
+                    }
+                    else if(resultado[lin][col] == 'D'){
+                        jogadores[lin].pontuacao += 1;
+                        erro = false;
+                    }
+                    else{
+                        printf("\nEntrada inválida!");
+                    }
+                }while(erro != false);
+            }
+        }
+    }
+
+    for(lin = 0; lin < 5; lin++){
+        for(col = 0; col < 5; col++){
+            
+        }
+    }
 }
