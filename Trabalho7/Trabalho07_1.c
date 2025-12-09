f24ee98aca80dc8faf1bfa8f0aec7ffc1929ca6b
marcelojunior03 <marceloheiffigjr@hotmail.com>
2025-12-09 20:39:24 -0300

    Update Trabalho07_1.c

--- Trabalho7/Trabalho07_1.c
+++ Trabalho7/Trabalho07_1.c
@@ -1,20 +1,18 @@
-//Nome: Marcelo Heiffig Júnior - RA: CV3095126
+//Nome: Marcelo Heiffig JÃºnior - RA: CV3095126
 
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 
-void upts(char *frase, char *outra){
+void upts(char *frase){
     int i;
-    int j = 0;
     for(i = 0; *(frase + i) != '\0'; i++){
         if((tolower(*(frase + i)) == 'a') ||
            (tolower(*(frase + i)) == 'e') ||
            (tolower(*(frase + i)) == 'i') ||
            (tolower(*(frase + i)) == 'o') ||
            (tolower(*(frase + i)) == 'u')){
-            *(outra+j) = *(frase+i);
-            j++;
+            printf("%c", *(frase + i));
         }
     }
 
@@ -26,19 +24,13 @@ void upts(char *frase, char *outra){
            (tolower(*(frase + i)) != 'u') &&
            (tolower(*(frase + i)) > 'a') &&
            (tolower(*(frase + i)) <= 'z')){
-            *(outra+j) = *(frase+i);
-            j++;
+            printf("%c", *(frase + i));
         }
     }
-
-    *(outra+j) = '\0';
 }
 
 void main(){
     char frase[100] = {"Oi Tudo bem?"};
-    char outraFrase[100];
 
-    upts(frase, outraFrase);
-
-    puts(outraFrase);
+    upts(frase);
 }
